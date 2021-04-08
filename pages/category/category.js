96fc50292e603f34eb9314e99802a80013788c01
habc706 <71514169+habc706@users.noreply.github.com>
2021-04-08 16:30:07 +0800

    new

--- pages/category/category.js
+++ pages/category/category.js
@@ -6,16 +6,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    test_Lenth:'',
+    height_Array:[{height:'150rpx', tips:1},{height:'150rpx', tips:1},{height:'150rpx', tips:1}],
+    test01:{   //把具体内容嵌套在list01里面
+      test_Color2:'#4285f4',
+      list01:[{
+        title_Name:'学习',
+        real_Content:[{content:'学习一下123Aa222222222222222bsssdrfeeszzzzaaqqqqqqasfghh'},{content:'学c++898'},{content:'uaaohu'}]
+      },{
+        title_Name:'运动',
+        real_Content:[{content:'学习一下123Aab'},{content:'学c++898'},{content:'uaaohu'}]
+      },{
+        title_Name:'生活',
+        real_Content:[{content:'学习一下123Aab'},{content:'学c++898'},{content:'uaaohu'}]
+      }]
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let lenth=this.data.test01.list01.length
     if (app.globalData.primaryColor) {
       this.setData({
-        primaryColor: app.globalData.primaryColor
+        primaryColor: app.globalData.primaryColor,
+        test_Lenth:lenth
       })
     }
   },
@@ -31,11 +47,37 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    let tabbar = this.getTabBar()
+    let tabbar = this.getTabBar()   
     tabbar.setData({
       btn2: `color:${this.data.primaryColor}`,
+     
     })
   },
+  switch_Page:function(e){
+    console.log(e)
+  },
+  zhankai:function(e){
+    var n=e.currentTarget.dataset.xushu
+    let tip=this.data.height_Array[n].tips
+    let content_len=this.data.test01.list01[n].real_Content.length
+    let gao=(content_len+1)*150
+    
+    let str = 'height_Array'+'['+n+']'+'.height'
+    let str2 = 'height_Array'+'['+n+']'+'.tips'
+    
+    if(tip>0){
+      this.setData({ 
+        [str]:gao+'rpx',
+        [str2]:-1
+        })
+    }else{
+      this.setData({
+        [str]:'150rpx',
+        [str2]:1
+    })
+    }
+  },
+
 
   /**
    * 生命周期函数--监听页面隐藏
